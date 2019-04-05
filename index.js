1a181878cf88be2fd405991a507e1e5ab4b70103
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-05 09:31:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+//Add a to b
+function add(a, b){
+  return (a+b)
+}
+// Subtract a and b 
+function subtract(a, b){
+return(a-b)
+}
+// return a multiplied by b 
+function multiply(a, b){
+  return(a*b)
+}
+// return a divided by b 
+function divide(a, b){
+  return(a/b)
+}
+// return n inclining 
+function inc(n) {
+  return (n += 1)
+}
+// return n declining 
+function dec(n){
+return (n -= 1)
+
+}
+//Return Parseint 
+function makeInt(n) {
+ return parseInt(n, 10);
+}
+
+function 
